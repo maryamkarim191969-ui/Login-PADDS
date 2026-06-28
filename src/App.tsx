d03405303634a4b40e98bb9357e8550b62cb09b7
maryamkarim191969-ui <maryamkarim191969@gmail.com>
2026-06-29 06:11:31 +0800

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import { LoginPage } from './pages/LoginPage';
+export function App() {
+  return <LoginPage />;
+}
---
+++ src/canvas.manifest.js
@@ -0,0 +1,5 @@
+export const manifest = {
+  screens: {
+    scr_o2ybbz: { name: "Home", route: "/" }
+  }
+};
